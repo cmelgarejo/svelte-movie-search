10716482bde3797e822d3a690782421a9c97c1f3
cmelgarejo <cmelgarejo.dev@gmail.com>
2020-06-25 14:19:27 -0400

    cmoooooon

--- api/v1/test.go
+++ api/v1/test.go
@@ -9,5 +9,5 @@ import (
 // Test entry point of the slsfn /v{X}/Test
 func Test(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "%s - %s - %s", os.Getenv("PROVIDER_GOOGLE_KEY"), os.Getenv("PROVIDER_GOOGLE_SECRET"),
-		os.Getenv("VERCEL_URL"))
+		"https://svelte-movie-search-git-serverless.cmelgarejo.now.sh/api/v1/auth/callback")
 }
